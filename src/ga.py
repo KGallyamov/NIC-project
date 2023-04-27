a4a6b5d50b284bb93e9634e43f7e5e372560cc1a
mirak <mirakpjcppr@gmail.com>
2023-04-27 20:41:33 +0300

    Mutation draft

--- src/ga.py
+++ src/ga.py
@@ -1,3 +1,4 @@
+# Code for GA training is adapted from the labs
 import numpy as np
 from typing import List, Tuple
 
@@ -6,7 +7,7 @@ import torch.nn as nn
 
 from tqdm import tqdm
 import torch.utils.data as data_utils
-from model import AutoEncoder
+from model import AutoEncoder, ACTIVATIONS
 
 
 class GeneticAlgorithm:
@@ -15,15 +16,136 @@ class GeneticAlgorithm:
         self.train_loader = data_utils.DataLoader(train_data, batch_size=batch_size, shuffle=True)
         self.val_loader = data_utils.DataLoader(val_data, batch_size=batch_size, shuffle=False)
         self._device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.fitness = dict()
 
-    def mutate(self, x: List[str]) -> List[str]:
-        pass
+    def mutate(self, x: List[str], p: float) -> List[str]:
+        """
+        Given config of a single AE, mutate each layer with probability p
+        :param x: Original chromosome
+        :param p: Mutation chance
+        :return: Updated config
+        """
+        mutated_x = x.copy()
+        if np.random.random() < p:  # Change the activation function with probability p
+            mutated_x[0] = np.random.choice([act for act in ACTIVATIONS if act != x[0]])
+        action_prob = np.random.random()
+        # Insert a new layer with probability p / 3
+        if 0 <= action_prob <= p / 3 and len(mutated_x) > 2:
+            ind = np.random.randint(1, len(mutated_x) - 1)
+            mutated_x[ind], new_layer, mutated_x[ind + 1] = self._expand_layers(mutated_x[ind], mutated_x[ind + 1])
+            mutated_x.insert(ind, new_layer)
+            return mutated_x
+        # Delete a random layer with probability p / 3
+        elif p / 3 < action_prob < 2 * p / 3 and len(mutated_x) > 3:
+            ind = np.random.randint(2, len(mutated_x) - 2)
+            rm_layer = mutated_x[ind]
+            del mutated_x[ind]
+            mutated_x[ind - 1], mutated_x[ind] = self._compress_layers(mutated_x[ind - 1],
+                                                                       rm_layer, mutated_x[ind])
+            return mutated_x
+        # Change the number of neurons in a random layer, again, with probability p / 3
+        elif action_prob < p:
+            ind = np.random.randint(1, len(mutated_x))
+            mutated_x[ind] = self._alter_layer(mutated_x[ind])
+        return mutated_x
+
+    def _compress_layers(self, l1: str, l2: str, l3: str) -> Tuple[str, str]:
+        return None
+
+    def _expand_layers(self, l1: str, l2: str) -> Tuple[str, str, str]:
+        return None
+
+    def _alter_layer(self, layer: str) -> str:
+        return None
 
     def crossover(self, x1, x2) -> Tuple[List[str], List[str]]:
         pass
 
-    def train_ga(self, steps=100, population_size=20, epochs_per_sample=50):
-        pass
+    def compute_fitness(self, *args, **kwargs) -> float:
+        """
+        This function should be called only once for each model to optimize performance
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        return 0
+
+    def get_elite(self, generation: List[List[str]], k) -> List[List[str]]:
+        """
+        Return "k" most fit samples from the population
+        :param generation: List of Chromosomes
+        :param k: # of top samples
+        :return: List of Chromosomes of length "k"
+        """
+        generation = sorted(generation, key=lambda x: self.fitness[x])
+        return generation[-k:]
+
+    def _generate_population(self, k) -> List[List[str]]:
+        """
+        :param k: Size of the population
+        :return: "k" chromosomes
+        """
+        return None
+
+    def train_ga(self, k=30, n_trial=100, keep_parents=False, patience=3, mutation_p=0.2, epochs_per_sample=50):
+        """
+        Genetic Algorithm implementation
+        :param k: Population size
+        :param n_trial: Number of iterations
+        :param keep_parents: Elitism
+        :param patience: Parameter for early stopping
+        :return: The most fit individual after "n_trial"s
+        :param mutation_p: Probability of mutation
+        :param epochs_per_sample: The number of epochs a sample is trained on
+        Future improvement: train all models till convergence (from early stop)
+        """
+        # Generate initial population
+        gen = self._generate_population(k)
+
+        # Calculate the initial fitness
+        prev_fitness = self.fitness[gen[0]]
+
+        # Flag to stop if there is no improvements for some generations
+        early_stop_flag = patience
+        for _ in tqdm(range(n_trial)):
+            gen = self.get_elite(gen, k)
+            gen_fitness = self.fitness[gen[-1]]
+            # print('\nImprovement', gen_fitness - prev_fitness, 'New score:', gen_fitness)
+
+            early_stop_flag = early_stop_flag - 1 if prev_fitness - gen_fitness >= 0 else patience
+            if early_stop_flag == 0:
+                print('Early stop')
+                break
+            prev_fitness = gen_fitness
+
+            next_gen = []
+
+            # Elitism
+            if keep_parents:
+                next_gen.extend(gen)
+
+            # Cross over
+            for i in range(len(gen)):
+                for j in range(i + 1, len(gen)):
+                    c1, c2 = self.crossover(gen[i], gen[j])
+                    next_gen.append(c1)
+                    next_gen.append(c2)
+
+                c = self.mutate(gen[i], mutation_p)
+                next_gen.append(c)
+            gen = next_gen
+            # Train autoencoders encoded in current population and save their fitness
+            for chromosome in gen:
+                model, tr_history, vl_history = self._fit_autoencoder(chromosome, epochs_per_sample)
+                if chromosome not in self.fitness.keys():
+                    self.fitness[chromosome] = self.compute_fitness(model)
+                else:
+                    self.fitness[chromosome] = min(self.compute_fitness(model), self.fitness[chromosome])
+        # Get the best solution
+        top_chromosome = self.get_elite(gen, 1)[0]
+        top_model, top_model_train_losses, top_model_val_losses = self._fit_autoencoder(top_chromosome,
+                                                                                        epochs_per_sample)
+        return self.fitness[top_chromosome], top_model, top_model_train_losses, top_model_val_losses
 
     def _fit_autoencoder(self, cfg: List[str], epochs):
         model = AutoEncoder(cfg)
--- src/model.py
+++ src/model.py
@@ -1,12 +1,17 @@
-import numpy as np
-import torch
 import torch.nn as nn
 
-from typing import List
-from collections import OrderedDict
+from typing import List, Tuple
+
+ACTIVATIONS = ['ReLU', 'Tanh', 'Sigmoid', 'LReLU']
 
 
 def _resolve_act(activation: str) -> nn.Module:
+    """
+    Given activation name, resolve it into the corresponding object
+    :param activation: str, activation name
+    :return: Activation module
+    """
+    assert activation in ACTIVATIONS, f'Activation {activation} is not supported'
     act = None
     if activation.lower() == 'relu':
         act = nn.ReLU()
@@ -14,14 +19,18 @@ def _resolve_act(activation: str) -> nn.Module:
         act = nn.Tanh()
     elif activation.lower() == 'sigmoid':
         act = nn.Sigmoid()
-    elif activation.lower() == 'lrelu':
+    else:  # activation.lower() == 'lrelu':
         act = nn.LeakyReLU
-    else:
-        assert NotImplementedError, f'Activation {activation} is not supported'
     return act
 
 
-def _resolve_layer(layer_cfg: str, activation):
+def _resolve_layer(layer_cfg: str, activation: str) -> Tuple[List[nn.Module], List[nn.Module]]:
+    """
+    Given layer config, return the corresponding AE blocks for encoder and decoder
+    :param layer_cfg: str in the format layer_fanin_fanout (_kernelsize)
+    :param activation: Activation name encoding
+    :return:
+    """
     l_type = layer_cfg.split('_')[0]
     enc_layer, dec_layer = None, None
     fan_in, fan_out = map(int, layer_cfg.split('_')[1:3])
@@ -49,6 +58,9 @@ def _resolve_layer(layer_cfg: str, activation):
 
 class AutoEncoder(nn.Module):
     def __init__(self, cfg: List[str]):
+        """
+        :param cfg: List of str in the format: layertype_fanin_fanout (_kernelsize for layertype=conv)
+        """
         super().__init__()
         activation = cfg[0]
         encoder_list = []
@@ -59,6 +71,7 @@ class AutoEncoder(nn.Module):
             enc_layer = []
             dec_layer = []
             enc_, dec_ = _resolve_layer(layer_cfg, activation)
+            # Add symmetric layers to encoder and decoder
             enc_layer.append(enc_)
             dec_layer.append(dec_)
             if 'conv' in cfg[i - 1].lower() and 'linear' in cfg[i]:
@@ -76,7 +89,6 @@ class AutoEncoder(nn.Module):
         t = self.encoder(x)
         _x = self.decoder(t)
         return _x
-
 
 # if __name__ == '__main__':
 #     cfg_sample = ['ReLU', 'conv_3_32_3', 'conv_32_64_3']
